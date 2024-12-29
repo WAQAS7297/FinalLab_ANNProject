c8e1a1c5e7b7dcee099d88de5c1771d50dceedc1
Waqas Ramzan <waqasramzan88888@gmail.com>
2024-12-29 16:36:12 +0500

    added evaluator.py and evaluation_regression plus evaluate_kera_cnn are added in evaluator.py

---
+++ evaluator.py
@@ -0,0 +1,41 @@
+import torch
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_recall_fscore_support, mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class Evaluator:
+    def __init__(self, model_type):
+        self.model_type = model_type
+
+    def evaluate(self, model, **kwargs):
+        if self.model_type == 'ann_regressor':
+            return self._evaluate_regression(model, **kwargs)
+        elif self.model_type == 'ann_classifier':
+            return self._evaluate_classification(model, **kwargs)
+        elif self.model_type == 'keras_cnn':
+            return self._evaluate_keras_cnn(model, **kwargs)
+        else:
+            raise ValueError("Unsupported model type.")
+
+    def _evaluate_regression(self, model, X_val, y_val):
+        with torch.no_grad():
+            pred = model.forward(X_val)
+            mse = mean_squared_error(y_val.numpy(), pred.numpy())
+            mae = mean_absolute_error(y_val.numpy(), pred.numpy())
+            r2 = r2_score(y_val.numpy(), pred.numpy())
+
+        self._plot_regression_predictions(y_val.numpy(), pred.numpy())
+        return mse, mae, r2
+
+    def _evaluate_keras_cnn(self, model, X_val, y_val, history):
+        predictions = model.model.predict(X_val)
+        predicted_classes = np.argmax(predictions, axis=1)
+        accuracy = accuracy_score(y_val, predicted_classes) * 100
+        cm = confusion_matrix(y_val, predicted_classes)
+        precision, recall, f1, _ = precision_recall_fscore_support(y_val, predicted_classes, average='weighted')
+
+        self._plot_confusion_matrix(cm, "Keras CNN Classifier")
+        self._plot_keras_training_history(history)
+        return accuracy, cm, precision, recall, f1
